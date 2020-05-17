9c8b0fded25c669e18457d1bb354b5075952a756
ehime-covid <ehime.covid19@gmail.com>
2020-05-18 00:32:18 +0900

    東京の新機能とマージ

---
+++ components/cards/ConfirmedCasesDetailsCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-col cols="12" md="6" class="DataCard">
+    <data-view
+      :title="$t('検査陽性者の状況')"
+      :title-id="'details-of-confirmed-cases'"
+      :date="updatedAt"
+    >
+      <template v-slot:description>
+        <ul>
+          <li>
+            {{
+              $t('（注）県内において疑い例または患者の濃厚接触者として検査を行ったものについて掲載')
+            }}
+          </li>
+          <li>
+            {{
+              $t(
+                '（注）症状確認中は軽症・中等症に含む'
+              )
+            }}
+          </li>
+        </ul>
+      </template>
+      <confirmed-cases-details-table
+        :aria-label="$t('検査陽性者の状況')"
+        v-bind="confirmedCases"
+      />
+    </data-view>
+  </v-col>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+import Data from '@/data/data.json'
+import formatConfirmedCases from '@/utils/formatConfirmedCases'
+import DataView from '@/components/DataView.vue'
+import ConfirmedCasesDetailsTable from '@/components/ConfirmedCasesDetailsTable.vue'
+
+export default {
+  components: {
+    DataView,
+    ConfirmedCasesDetailsTable
+  },
+  data() {
+    // 検査陽性者の状況
+    const confirmedCases = formatConfirmedCases(Data.main_summary)
+
+    const updatedAt = dayjs(Data.main_summary.children[0].date).format(
+      'YYYY/MM/DD HH:mm'
+    )
+
+    const data = {
+      Data,
+      confirmedCases,
+      updatedAt
+    }
+    return data
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,262 +1,170 @@
 <template>
   <div class="MainPage">
-    <page-header
-      :icon="headerItem.icon"
-      :title="headerItem.title"
-      :date="headerItem.date"
-    />
+    <div class="Header mb-3">
+      <!--<page-header :icon="headerItem.icon">{{ headerItem.title }}</page-header>-->
+      <page-header :icon="headerItem.icon" :title="headerItem.title"/>
+      <div class="UpdatedAt">
+        <time :datetime="updatedAt">{{ Data.lastUpdate }}</time>
+      </div>
+      <div
+        v-show="!['ja', 'ja-basic'].includes($i18n.locale)"
+        class="Annotation"
+      >
+        <span>{{ $t('注釈') }}</span>
+      </div>
+    </div>
     <whats-new class="mb-4" :items="newsItems" />
     <static-info
       class="mb-4"
-      :url="'/flow'"
-      :text="'自分や家族の症状に不安や心配があればまずは電話相談をどうぞ'"
-      :btn-text="'相談の手順を見る'"
+      :url="localePath('/flow')"
+      :text="$t('自分や家族の症状に不安や心配があればまずは電話相談をどうぞ')"
+      :btn-text="$t('相談の手順を見る')"
     />
-    <v-row class="DataBlock">
-      <v-col cols="12" md="6" class="DataCard">
-        <svg-card
-          title="検査陽性者の状況"
-          :title-id="'details-of-confirmed-cases'"
-          :date="Data.inspections_summary.date"
-        >
-          <confirmed-cases-table v-bind="confirmedCases" />
-        </svg-card>
-      </v-col>
-      <v-col cols="12" md="6" class="DataCard">
-        <time-bar-chart
-          title="陽性患者数"
-          :title-id="'number-of-confirmed-cases'"
-          :chart-id="'time-bar-chart-patients'"
-          :chart-data="patientsGraph"
-          :date="Data.patients.date"
-          :unit="'人'"
-          :url="
-            'https://www.pref.ehime.jp/h25500/kansen/covid19.html#kansensha'
-          "
-        />
-      </v-col>
-      <v-col cols="12" md="6" class="DataCard">
-        <data-table
-          :title="'陽性患者の属性'"
-          :title-id="'attributes-of-confirmed-cases'"
-          :chart-data="patientsTable"
-          :chart-option="{}"
-          :date="Data.patients.date"
-          :info="sumInfoOfPatients"
-          :url="
-            'https://www.pref.ehime.jp/h25500/kansen/covid19.html#kansensha'
-          "
-        />
-      </v-col>
-      <v-col cols="12" md="6" class="DataCard">
-        <time-stacked-bar-chart
-          title="検査実施数"
-          :title-id="'number-of-tested'"
-          :chart-id="'time-stacked-bar-chart-inspections'"
-          :chart-data="inspectionsGraph"
-          :date="Data.inspections_summary.date"
-          :items="inspectionsItems"
-          :labels="inspectionsLabels"
-          :unit="'件'"
-        />
-      </v-col>
-      <!--XXXXXXX
-      <v-col cols="12" md="6" class="DataCard">
-        <time-bar-chart
-          title="新型コロナコールセンター相談件数"
-          :title-id="'number-of-reports-to-covid19-telephone-advisory-center'"
-          :chart-id="'time-bar-chart-contacts'"
-          :chart-data="contactsGraph"
-          :date="Data.contacts.date"
-          :unit="'件'"
-          :url="''"
-        />
-      </v-col>
-      <v-col cols="12" md="6" class="DataCard">
-        <time-bar-chart
-          title="新型コロナ受診相談窓口相談件数"
-          :title-id="'number-of-reports-to-covid19-consultation-desk'"
-          :chart-id="'time-bar-chart-querents'"
-          :chart-data="querentsGraph"
-          :date="Data.querents.date"
-          :unit="'件'"
-          :url="''"
-        />
-      </v-col>
-      <v-col cols="12" md="6" class="DataCard">
-        <metro-bar-chart
-          title="都営地下鉄の利用者数の推移"
-          :title-id="'predicted-number-of-toei-subway-passengers'"
-          :chart-id="'metro-bar-chart'"
-          :chart-data="metroGraph"
-          :chart-option="metroGraphOption"
-          :date="metroGraph.date"
-        />
-      </v-col>
--->
-    </v-row>
+    <card-row class="DataBlock">
+      <!-- 検査陽性者の状況 -->
+      <confirmed-cases-details-card />
+      <!-- 検査実施状況 -->
+      <tested-cases-details-card />
+      <!-- 陽性患者数 -->
+      <confirmed-cases-number-card />
+      <!-- 陽性患者数（検査結果判明日別） -->
+      <!--<positive-number-by-diagnosed-date-card />-->
+      <!-- 陽性患者の属性 -->
+      <confirmed-cases-attributes-card />
+      <!-- 区市町村別患者数 -->
+      <!--<confirmed-cases-by-municipalities-card />-->
+      <!-- 陽性率グラフ-->
+      <!--<positive-rate-card />-->
+      <!--<v-col class="DesktopSpacer" />-->
+      <!-- 検査実施人数 -->
+      <inspection-persons-number-card />
+      <!-- 検査実施件数 -->
+      <!--<tested-number-card />-->
+      <!-- 新型コロナコールセンター相談件数 -->
+      <!--<telephone-advisory-reports-number-card />-->
+      <!-- 新型コロナ受診相談窓口相談件数 -->
+      <!--<consultation-desk-reports-number-card />-->
+      <!-- 都営地下鉄の利用者数の推移 -->
+      <!--<metro-card />-->
+      <!-- 都庁来庁者数の推移 -->
+      <!--<agency-card />-->
+    </card-row>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from 'vue'
+import { MetaInfo } from 'vue-meta'
 import PageHeader from '@/components/PageHeader.vue'
-import TimeBarChart from '@/components/TimeBarChart.vue'
-// import MetroBarChart from '@/components/MetroBarChart.vue'
-import TimeStackedBarChart from '@/components/TimeStackedBarChart.vue'
 import WhatsNew from '@/components/WhatsNew.vue'
 import StaticInfo from '@/components/StaticInfo.vue'
+import CardRow from '@/components/cards/CardRow.vue'
 import Data from '@/data/data.json'
-import MetroData from '@/data/metro.json'
-import DataTable from '@/components/DataTable.vue'
-import formatGraph from '@/utils/formatGraph'
-import formatTable from '@/utils/formatTable'
-import formatConfirmedCases from '@/utils/formatConfirmedCases'
 import News from '@/data/news.json'
-import SvgCard from '@/components/SvgCard.vue'
-import ConfirmedCasesTable from '@/components/ConfirmedCasesTable.vue'
+import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsCard.vue'
+import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
+import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttributesCard.vue'
+import ConfirmedCasesByMunicipalitiesCard from '@/components/cards/ConfirmedCasesByMunicipalitiesCard.vue'
+import TestedCasesDetailsCard from '@/components/cards/TestedCasesDetailsCard.vue'
+import InspectionPersonsNumberCard from '@/components/cards/InspectionPersonsNumberCard.vue'
+import TestedNumberCard from '@/components/cards/TestedNumberCard.vue'
+import TelephoneAdvisoryReportsNumberCard from '@/components/cards/TelephoneAdvisoryReportsNumberCard.vue'
+import ConsultationDeskReportsNumberCard from '@/components/cards/ConsultationDeskReportsNumberCard.vue'
+import PositiveRateCard from '~/components/cards/PositiveRateCard.vue'
+import MetroCard from '@/components/cards/MetroCard.vue'
+import AgencyCard from '@/components/cards/AgencyCard.vue'
+import PositiveNumberByDiagnosedDateCard from '@/components/cards/PositiveNumberByDiagnosedDateCard.vue'
+import { convertDatetimeToISO8601Format } from '@/utils/formatDate'
 
-export default {
+export default Vue.extend({
   components: {
+    PositiveRateCard,
     PageHeader,
-    TimeBarChart,
-    // MetroBarChart,
-    TimeStackedBarChart,
     WhatsNew,
     StaticInfo,
-    DataTable,
-    SvgCard,
-    ConfirmedCasesTable
+    CardRow,
+    ConfirmedCasesDetailsCard,
+    ConfirmedCasesNumberCard,
+    ConfirmedCasesAttributesCard,
+    ConfirmedCasesByMunicipalitiesCard,
+    TestedCasesDetailsCard,
+    InspectionPersonsNumberCard,
+    TestedNumberCard,
+    TelephoneAdvisoryReportsNumberCard,
+    ConsultationDeskReportsNumberCard,
+    MetroCard,
+    AgencyCard,
+    PositiveNumberByDiagnosedDateCard
   },
   data() {
-    // 感染者数グラフ
-    const patientsGraph = formatGraph(Data.patients_summary.data)
-    // 感染者数
-    const patientsTable = formatTable(Data.patients.data)
-    // 退院者グラフ
-    const dischargesGraph = formatGraph(Data.discharges_summary.data)
-
-    // 相談件数
-    const contactsGraph = formatGraph(Data.contacts.data)
-    // 帰国者・接触者電話相談センター相談件数
-    const querentsGraph = formatGraph(Data.querents.data)
-    // 都営地下鉄の利用者数の推移
-    const metroGraph = MetroData
-    // 検査実施日別状況
-    const inspectionsGraph = [
-      Data.inspections_summary.data['県内'],
-      Data.inspections_summary.data['その他']
-    ]
-    const inspectionsItems = ['疑い例・接触者調査', 'その他']
-    const inspectionsLabels = Data.inspections_summary.labels
-    // 死亡者数
-    // #MEMO: 今後使う可能性あるので一時コメントアウト
-    // const fatalitiesTable = formatTable(
-    //   Data.patients.data.filter(patient => patient['備考'] === '死亡')
-    // )
-    // 検査陽性者の状況
-    const confirmedCases = formatConfirmedCases(Data.main_summary)
-
-    const sumInfoOfPatients = {
-      lText: patientsGraph[
-        patientsGraph.length - 1
-      ].cumulative.toLocaleString(),
-      sText: patientsGraph[patientsGraph.length - 1].label + 'の累計',
-      unit: '人'
-    }
-
     const data = {
       Data,
-      patientsTable,
-      patientsGraph,
-      dischargesGraph,
-      contactsGraph,
-      querentsGraph,
-      metroGraph,
-      inspectionsGraph,
-      inspectionsItems,
-      inspectionsLabels,
-      confirmedCases,
-      sumInfoOfPatients,
       headerItem: {
         icon: 'mdi-chart-timeline-variant',
-        title: '愛媛県内の最新感染動向',
-        date: Data.lastUpdate
+        title: this.$t('愛媛県内の最新感染動向')
       },
-      newsItems: News.newsItems,
-      metroGraphOption: {
-        responsive: true,
-        legend: {
-          display: true
-        },
-        scales: {
-          xAxes: [
-            {
-              position: 'bottom',
-              stacked: false,
-              gridLines: {
-                display: true
-              },
-              ticks: {
-                fontSize: 10,
-                maxTicksLimit: 20,
-                fontColor: '#808080'
-              }
-            }
-          ],
-          yAxes: [
-            {
-              stacked: false,
-              gridLines: {
-                display: true
-              },
-              ticks: {
-                fontSize: 12,
-                maxTicksLimit: 10,
-                fontColor: '#808080',
-                callback(value) {
-                  return value.toFixed(2) + '%'
-                }
-              }
-            }
-          ]
-        },
-        tooltips: {
-          displayColors: false,
-          callbacks: {
-            title(tooltipItems, _) {
-              const label = tooltipItems[0].label
-              return `期間: ${label}`
-            },
-            label(tooltipItem, data) {
-              const currentData = data.datasets[tooltipItem.datasetIndex]
-              const percentage = `${currentData.data[tooltipItem.index]}%`
-
-              return `${metroGraph.base_period}の利用者数との相対値: ${percentage}`
-            }
-          }
-        }
-      }
+      newsItems: News.newsItems
     }
     return data
   },
-  head() {
+  computed: {
+    updatedAt() {
+      return convertDatetimeToISO8601Format(this.$data.Data.lastUpdate)
+    }
+  },
+  head(): MetaInfo {
     return {
-      title: '愛媛県内の最新感染動向'
+      title: this.$t('愛媛県内の最新感染動向') as string
     }
   }
-}
+})
 </script>
 
 <style lang="scss" scoped>
 .MainPage {
+  .Header {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-end;
+
+    @include lessThan($small) {
+      flex-direction: column;
+      align-items: baseline;
+    }
+  }
+
+  .UpdatedAt {
+    @include font-size(14);
+
+    color: $gray-3;
+    margin-bottom: 0.2rem;
+  }
+
+  .Annotation {
+    @include font-size(12);
+
+    color: $gray-3;
+    @include largerThan($small) {
+      margin: 0 0 0 auto;
+    }
+  }
   .DataBlock {
     margin: 20px -8px;
+
     .DataCard {
       @include largerThan($medium) {
         padding: 10px;
       }
+
       @include lessThan($small) {
         padding: 4px 8px;
+      }
+    }
+
+    .DesktopSpacer {
+      padding: 0;
+      @media screen and (min-width: 960px) {
+        padding: 10px;
       }
     }
   }
